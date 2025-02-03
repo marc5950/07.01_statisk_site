ca2bd0c638141d3dd4330e483af3a25eb0faf157
Marcus Wagner <marcuswagner5950@gmail.com>
2025-02-03 15:10:06 +0100

    menu dynamisk

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const menu = urlParams.get("menu");
+
+console.log("menu;", menu);
+
+document.querySelector(".katliste").innerHTML = `<h2>${menu}</h2> <section class="kategorier"></section>`;
+
 let katListe = document.querySelector(".kategorier");
-fetch(`https://kea-alt-del.dk/t7/api/categories`)
+fetch(`https://kea-alt-del.dk/t7/api/${menu}`)
   .then((response) => response.json())
   .then((data) => showData(data));
 
+function menukat(menu) {
+  if (menu === "Categories") {
+    return "category";
+  } else if (menu === "Subcategories") {
+    return "subcategory";
+  } else if (menu === "Seasons") {
+    return "season";
+  }
+}
+
 function showData(data) {
   console.log(data);
-  const markup = data.map((data) => `<a href="produktliste.html?kategori=${data.category}">${data.category}</a>`).join(``);
+  const kategoriNavn = menukat(menu);
+  const markup = data
+    .map(
+      (data) => `<a href="produktliste.html?kategori=${data[kategoriNavn]}">${data[kategoriNavn]}</a>
+  `
+    )
+    .join(``);
   katListe.innerHTML = markup;
 }
